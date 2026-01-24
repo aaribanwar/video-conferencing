932002bc3a633a4f08ccebe6a860bba750b8b20c
aaribanwar <aaribactivism@gmail.com>
2026-01-24 23:53:14 +0530

    started express

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,20 @@ import mongoose from "mongoose";
 import {createServer} from "node:http";
 // import cors form "cors";
 import {Server} from "socket.io";
-
+import 'dotenv/config';
 
 
 const app = express();
+const port = process.env.PORT;
 
 app.get("/home", (req,res) => {
     return res.json({"hello":"world"});
 });
+
+app.listen(port || 3000, () => {
+    console.log("listening on port:", port || 3000);
+});
+
 
 
 
